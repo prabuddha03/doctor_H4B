04616cef18d0c92a4259bf700652c92110db46c1
Supriyo Saha <mm.saha1982@gmail.com>
2023-07-07 18:38:27 +0530

    small fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
        <p className="text-8xl font-bold pt-4 text-[#a72121]">Almost 12,000</p>
        <p className="text-8xl font-bold pt-4 text-[#cccccc]">People Die</p>
        <p className="text-4xl pt-4 text-[#d43f3f]">due to unavailability of blood </p>
-       <div className="flex pt-10">
+       <div className="flex pt-20">
        <button className="btn btn-outline w-[600px]">Find yourself a Blood Donor</button>
        <button className="btn btn-info mx-10 w-[200px]">explore</button>
        </div>
