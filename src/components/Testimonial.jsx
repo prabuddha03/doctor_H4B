a8ca090b98c6df8d3b0414a0960b50041acf7ae6
Supriyo Saha <mm.saha1982@gmail.com>
2023-07-07 19:59:08 +0530

    testimonials added

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Testimonial = ({ topHeading, middleText, bottomHeading}) => {
+  return (
+    <div className='text-[#00A8D0] py-20 text-center'>
+      <p className='text-5xl py-7 font-normal'>{topHeading}</p>
+      <p className='text-6xl font-extrabold pb-5'>{middleText}</p>
+      <p className='text-5xl font-medium'>{bottomHeading}</p>
+
+    </div>
+  )
+}
+
+export default Testimonial
