dc5117f762b5cf8a8eb7d0c5041feb68cdc0bf95
Codersubhra <subhrajitchatterjee251@gmail.com>
2023-07-08 13:15:39 +0530

    button fixed

--- src/components/ProfileSidebar.jsx
+++ src/components/ProfileSidebar.jsx
@@ -7,7 +7,7 @@ const ProfileSidebar = () => {
   <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content flex flex-col items-center justify-center">
    
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+    <label for="my-drawer-2" class="btn btn-wide btn-outline drawer-button lg:hidden">Open drawer</label>
   
   </div> 
   <div class="drawer-side">
