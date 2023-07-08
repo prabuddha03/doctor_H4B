b3888c5ce020b99191f4114dd968b9cafe9f40e3
Supriyo Saha <mm.saha1982@gmail.com>
2023-07-08 12:51:47 +0530

    primary drawer added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import NavbarSec from "../components/NavbarSec";
+import ProfileSidebar from "../components/ProfileSidebar";
 
 const Profile = () => {
   return (
     <div>
-      profile
+      <NavbarSec />
+      <div>
+        <ProfileSidebar />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
